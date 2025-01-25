84174ac90582539ccc4b1f78af0c01a1eb7bdeb7
White ray <white.ray.haha@gmail.com>
2025-01-25 19:27:39 +0500

    Add new files and update MyApplication.java

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,22 +1,67 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import db.CityDatabase;
+import models.City;
 import models.Weather;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class MyApplication {
     public static void main(String[] args) {
-        String token = "wj41qlc7ukds7tj5n2iisohhfxu843nhepoxmpz8";
+        CityDatabase db = new CityDatabase();
+        db.connect("jdbc:postgresql://localhost:5432/" + "postgres", "postgres", "0000");
         Scanner sc = new Scanner(System.in);
-        String place_id = "nursultan";
-        Weather weather = getWeather(token, place_id);
-        System.out.println(weather);
+        String place_id;
+        System.out.println("Welcome");
+        boolean t = true;
+        while(t){
+            try{
+                System.out.println("[1] View all cities");
+                System.out.println("[2] Get cities by id");
+                System.out.println("[3] Exit");
+
+                int choice = sc.nextInt();
+                switch(choice){
+                    case 1:
+                        ArrayList<City> cities = db.getAllCities();
+                        for(City city : cities){
+                            city.displayInfo();
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Enter ID: ");
+                        place_id = db.getCityById(sc.nextInt());
+                        String token = "wj41qlc7ukds7tj5n2iisohhfxu843nhepoxmpz8";
+                        Weather weather = getWeather(token, place_id);
+                        System.out.println(weather);
+                        break;
+                    case 3:
+                        t = false;
+                        break;
+                    default:
+                        System.out.println("Please select option between 1-3.");
+                        break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Input must be integer " + e.getMessage());
+
+            }catch(Exception e){
+                System.out.println("Exception " + e.getMessage());
+            }
+        }
+
+
+//        String token = "wj41qlc7ukds7tj5n2iisohhfxu843nhepoxmpz8";
+//        Weather weather = getWeather(token, place_id);
+//
+//        System.out.println(weather);
 
     }
 
@@ -74,9 +119,9 @@ public class MyApplication {
                         break;
                 }
                 return weather;
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e){
                 System.out.println(e.getMessage());
-        }catch(Exception e){
+        } catch(Exception e){
                 System.out.println(e.getMessage());
             }
 
