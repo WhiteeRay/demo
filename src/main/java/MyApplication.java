3996ace6d65e112d0a6c830006465b2af1fc3d5d
White ray <white.ray.haha@gmail.com>
2025-02-10 22:06:01 +0500

    first commit

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,16 +1,10 @@
 package service;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import db.CityDatabase;
 import models.City;
 import models.Weather;
 import repositories.WeatherRepository;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
